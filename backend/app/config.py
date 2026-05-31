5bd0f65c09784d357ac33599821bc2a99e124793
yoonSER <yoonseo1743@gmail.com>
2026-06-01 00:33:36 +0900

    init favorite market

--- backend/app/config.py
+++ backend/app/config.py
@@ -2,7 +2,7 @@ import os
 
 
 APP_NAME = "CoinSight"
-MAX_FAVORITE_MARKETS = int(os.getenv("MAX_FAVORITE_MARKETS", "7"))
+MAX_FAVORITE_MARKETS = int(os.getenv("MAX_FAVORITE_MARKETS", "5"))
 ANALYSIS_CACHE_MINUTES = int(os.getenv("ANALYSIS_CACHE_MINUTES", "30"))
 JWT_SECRET = os.getenv("JWT_SECRET", "change-this-secret-before-deploy")
 JWT_ALGORITHM = "HS256"
